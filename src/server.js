620bbaee085e43e98714e4e90700f1841ba122f1
jonesCode <jaboone26@outlook.es>
2019-09-06 22:06:55 +0200

    Actualizacion para desplegar en heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/recetarioApi'));
+app.use(express.static(__dirname+"../dist/recetarioApi"));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/recetarioApi/index.html'));
+    res.sendFile(path.join(__dirname+'../dist/recetarioApi/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
+
